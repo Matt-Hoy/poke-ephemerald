3be464c48c7fe4ff4830e1650182a3b9a76fa679
Matt-Hoy <mattdhoy@gmail.com>
2025-05-10 18:47:30 -0500

    party menu bug fix

--- src/party_menu.c
+++ src/party_menu.c
@@ -1490,11 +1490,11 @@ static void Task_ClosePartyMenuAndSetCB2(u8 taskId)
             if (VarGet(VAR_USING_KEYITEM) == 1)
             {
                 VarSet(VAR_USING_KEYITEM, 0);
-                SetMainCallback2(CB2_ReturnToField);
+                SetMainCallback2(gPartyMenu.exitCallback);
             }
             else
             {
-                SetMainCallback2(CB2_ReturnToField);
+                SetMainCallback2(gPartyMenu.exitCallback);
             }
 
         ResetSpriteData();
